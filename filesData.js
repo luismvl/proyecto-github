4068550238d0be0f4dd2b4d9ae305a07f02a4770
Luis Vela <luismvl114@gmail.com>
2021-11-08 10:08:47 -0400

    mejora haciendo las petiones http en paralelo

--- filesData.js
+++ filesData.js
@@ -1,77 +1,61 @@
 const { fetchContents } = require('./github-requests');
 
-const getFilesExt = async (files) => {
-  let extensionsCounter = {};
-  for (const file of files) {
-    if (file.type === 'file') {
-      //Registra la extension
-      const extension = file.name.match(/\.\w+$/);
-      // Si ya se registro la extension, suma 1 al contador
-      if (extensionsCounter[extension]) {
-        extensionsCounter[extension]++;
-      } else {
-        // Si aun no se habia registrado esa extension, crea su contador en 1
-        extensionsCounter[extension] = 1;
-      }
-    } else if (file.type === 'dir') {
-      // Obtiene los ficheros dentro del directorio
-      const filesInsideDir = await fetchContents(file.url);
-      const extCounterInsideDir = await getFilesExt(filesInsideDir);
-      // Une las extensiones registradas dentro el directorio con las primeras
-      for (const ext in extCounterInsideDir) {
-        if (extensionsCounter[ext]) {
-          extensionsCounter[ext] += extCounterInsideDir[ext];
-        } else {
-          extensionsCounter[ext] = extCounterInsideDir[ext];
-        }
-      }
-      // Une los archivos del directorio al primer array
-    }
-  }
-  return extensionsCounter;
-};
-
-const getFilesLines = async (files) => {
-  return Promise.all(files.map(async (file) => {
-    if (file.type === 'file') {
-      // Si no se puede descargar el archivo, no calcula el nro de lineas
-      if (!file.download_url) {
-        return {
-          name: file.path,
-          lines: 'file is empty',
-        };
-      }
-      let raw = await fetchContents(file.download_url);
-      console.log(`Procesando fichero '${file.name}'`);
-      // Si un archivo es json axios lo parsea a objeto de js
-      // En tal caso, lo convierto de nuevo a string
-      if (typeof raw === 'object') {
-        raw = JSON.stringify(raw, null, 3);
-      }
-      const lines = raw.split(/\n/g).length;
-      return {
-        name: file.path,
-        lines,
-      };
-    } else if (file.type === 'dir') {
-      const filesInsideDir = await fetchContents(file.url);
-      const linesCounterInsideDir = await getFilesLines(filesInsideDir);
-      return {
-        dirName: file.name,
-        files: linesCounterInsideDir,
-      };
-    }
-  }));
-};
-
 exports.getFilesData = async (files) => {
   if (!Array.isArray(files)) return null;
 
   let extensionsCounter = {};
   let linesCounter = [];
-  linesCounter = await getFilesLines(files);
-  extensionsCounter = await getFilesExt(files)
 
+  await Promise.all(
+    files.map(async (file) => {
+      if (file.type === 'file') {
+        //Registra la extension
+        const extension = file.name.match(/\.\w+$/);
+        // Si ya se registro la extension, suma 1 al contador
+        if (extensionsCounter[extension]) {
+          extensionsCounter[extension]++;
+        } else {
+          // Si aun no se habia registrado esa extension, crea su contador en 1
+          extensionsCounter[extension] = 1;
+        }
+
+        // Si no se puede descargar el archivo, no calcula el nro de lineas
+        if (!file.download_url) {
+          return;
+        }
+
+        console.log(`Procesando fichero '${file.name}'`);
+        let raw = await fetchContents(file.download_url);
+        // Si un archivo es json axios lo parsea a objeto de js
+        // En tal caso, lo convierto de nuevo a string
+        if (typeof raw === 'object') {
+          raw = JSON.stringify(raw, null, '\t');
+        }
+        const lines = raw.split(/\n/g).length;
+        linesCounter.push({
+          name: file.path,
+          lines,
+        });
+      } else if (file.type === 'dir') {
+        // Obtiene los ficheros dentro del directorio
+        const filesInsideDir = await fetchContents(file.url);
+
+        const filesDataInsideDir = await this.getFilesData(filesInsideDir);
+        const extCounterInsideDir = filesDataInsideDir.filesExts;
+        const linesCounterInsideDir = filesDataInsideDir.files;
+        // Une las extensiones registradas dentro el directorio con las primeras
+        for (const ext in extCounterInsideDir) {
+          if (extensionsCounter[ext]) {
+            extensionsCounter[ext] += extCounterInsideDir[ext];
+          } else {
+            extensionsCounter[ext] = extCounterInsideDir[ext];
+          }
+        }
+        // Une los ficheros dentro del directorio con el primer array
+        linesCounter = [...linesCounter, ...linesCounterInsideDir];
+      }
+    })
+  );
   return {
     filesExts: extensionsCounter,
     files: linesCounter,
